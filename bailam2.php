7077cb89e258f55be4e32fc71cdc054a4c19262d
laihung53 <laihung5376@gmail.com>
2017-03-08 17:51:32 +0700

    baitap4

---
+++ bailam2.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+	<title> tạo trang image </title>
+	<meta charet"utf-8";/>
+	<link rel="stylesheet" type="text/css" href="bailam2.css">
+	
+</head>
+<body > 
+		<h1><img class"123" src="0090.gif" width="50px" height="50px" > </h1>
+		<ul> 
+			<li> <a href="https://www.google.com.vn/?gws_rd=ssl"> trang chủ </a> </li>
+			<li><a href="https://www.baomoi.com">tìm kiếm </a></li>
+		</ul><br>
+		<h2> anbum ảnh  </h2>
+		<div class="style-image">
+			<a class="abc" href="#"><img class="first" src="123.jpg"></a></br>
+			<img src="555.jpg" alt="ảnh đẹp" title="ảnh">
+			<img src="666.jpg" alt="ảnh đẹp" title="ảnh">
+			<img src="777.jpg" alt="ảnh đẹp" title="ảnh">
+			<img src="15.jpg" alt="ảnh đẹp" title="ảnh">
+			<img src="16.jpg" alt="ảnh đẹp" title="ảnh">
+		</div>
+		<div id="chat">
+			<img src="bieutuong.jpg">
+		</div>
+
+
+
+</body>
+</html>
+
+</html>
